b79af01fbed0b1cbd6140a26b0b1f72351cc3419
Daniel Jones <daniel.jones2000@gmail.com>
2019-08-16 09:18:33 -0700

    laying out styling

--- starwars/src/App.jsx
+++ starwars/src/App.jsx
@@ -1,8 +1,21 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
 import axios from "axios";
+import PersonCard from "./components/cards";
+import styled from "styled-components";
 
-
+const SiteStyle = styled.div`
+width:1024px;
+margin: 0 auto;
+  .Header{
+    text-align:center;
+  }
+`;
+const AppStyle = styled.div`
+display:flex;
+flex-wrap:wrap;
+border:1px orange solid;
+`;
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
@@ -11,27 +24,29 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
-  const [person, setPerson] = useState();
+  // const [person, setPerson] = useState();
 
-  useEffect(() => {
-    axios.get('https://swapi.co/api/people/')
-    .then(response => {
-      console.log(response.data);
-      const starData = response.data;
-      setPerson(starData);
-    })
-    .catch(error =>{
-      console.log("Error: ", error)
-    })
+  // useEffect(() => {
+  //   axios.get('https://swapi.co/api/people/')
+  //   .then(response => {
+  //     console.log(response.data);
+  //     const starData = response.data;
+  //     setPerson(starData);
+  //   })
+  //   .catch(error =>{
+  //     console.log("Error: ", error)
+  //   })
 
-  }, [])
+  // }, [])
 
   return (
-    <div className="sitewrapper">
-      <div className="App">
-        <h1 className="Header">React Wars</h1>
-      </div>
-    </div>
+    <SiteStyle className="sitewrapper">
+      <h1 className="Header">React Wars</h1>
+        <AppStyle className="App">
+
+          <PersonCard/>
+        </AppStyle>
+    </SiteStyle>
   );
 }
 
--- starwars/src/components/cards.jsx
+++ starwars/src/components/cards.jsx
@@ -1,19 +1,30 @@
 import React from "react";
-// import styled from "styled-components";
+import styled from "styled-components";
+
+const CardStyle = styled.div`
+border:2px black solid;
+background: grey;
+width:200px
+    h2{
+        color: #443e3e;
+        text-shadow: 1px 1px 5px #fff;
+    }
+    p{
+        font-size: 1.25rem;
+    }
+`;
 
 const PersonCard = () =>{
     return(
-        <div className="person-card">
+        <CardStyle className="person-card">
             <h2 className="title">
                 Name
             </h2>
-            <ul>
-                <li>birth</li>
-                <li>gender</li>
-                <li>haircolor</li>
-                <li>height</li>
-            </ul>
-        </div>
+                <p>birth</p>
+                <p>gender</p>
+                <p>haircolor</p>
+                <p>height</p>
+        </CardStyle>
     )
 }
 export default PersonCard;
